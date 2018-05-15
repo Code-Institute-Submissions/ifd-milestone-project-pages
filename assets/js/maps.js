a21586cc4c8daf25fc5e612614a92b8a3b8e9e55
Ben Hasselgren <ben.nadalito@gmail.com>
2018-05-15 10:37:04 +0000

    added jquery file

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,7 +1,3 @@
-// This example requires the Places library. Include the libraries=places
-// parameter when you first load the API. For example:
-// <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
-
 var map, places, infoWindow;
 var markers = [];
 var autocomplete;
@@ -73,6 +69,7 @@ function reset() {
  map.setZoom(2);
  map.setCenter(countries["uk"].center);
  map.componentRestrictions = { 'country': [] };
+ place = "";
 
 }
 
